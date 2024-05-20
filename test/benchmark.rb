290a1c86e71324e975172ee53ff2c98a30784dee
digitalMoksha <brett@digitalmoksha.com>
2024-05-20 09:16:12 -0500

    Tigthen up benchmark output

--- test/benchmark.rb
+++ test/benchmark.rb
@@ -14,11 +14,11 @@ def compare_sanitize
   sanitize_config = Sanitize::Config::RELAXED
   [[DOCUMENT_HUGE, "huge"], [DOCUMENT_MEDIUM, "medium"], [DOCUMENT_SMALL, "small"]].each do |(html, label)|
     Benchmark.ips do |x|
-      x.report("sanitize-document-#{label}") do
+      x.report("sanitize-#{label}") do
         Sanitize.document(html, sanitize_config)
       end
 
-      x.report("selma-document-#{label}") do
+      x.report("selma-#{label}") do
         sanitizer = Selma::Sanitizer.new(Selma::Sanitizer::Config::RELAXED)
         Selma::Rewriter.new(sanitizer: sanitizer).rewrite(html)
       end
