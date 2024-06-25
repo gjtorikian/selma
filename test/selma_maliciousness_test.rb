8b2e2682c7025c270e44651c4462cd9aacf11c2b
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2024-06-25 18:31:53 -0400

    add testing for big strings

--- test/selma_maliciousness_test.rb
+++ test/selma_maliciousness_test.rb
@@ -167,4 +167,67 @@ class SelmaMaliciousnessTest < Minitest::Test
       Selma::Rewriter.new(sanitizer: sanitizer).rewrite(html)
     end
   end
+
+  class RemoveLinkClass
+    SELECTOR = Selma::Selector.new(match_element: %(a:not([class="anchor"])))
+
+    def selector
+      SELECTOR
+    end
+
+    def handle_element(element)
+      element.remove_attribute("class")
+    end
+  end
+
+  class RemoveIdAttributes
+    SELECTOR = Selma::Selector.new(match_element: %(a[id], li[id]))
+
+    def selector
+      SELECTOR
+    end
+
+    def handle_element(element)
+      # footnote ids should not be removed
+      return if element.tag_name == "li"
+      return if element.tag_name == "a"
+
+      # links with generated header anchors should not be removed
+      return if element.tag_name == "a" && element["class"] == "anchor"
+
+      element.remove_attribute("id")
+    end
+  end
+
+  class BaseRemoveRel
+    SELECTOR = Selma::Selector.new(match_element: %(a))
+
+    def selector
+      SELECTOR
+    end
+
+    def handle_element(element)
+      # we allow rel="license" to support the Rel-license microformat
+      # http://microformats.org/wiki/rel-license
+      unless element["rel"] == "license"
+        element.remove_attribute("rel")
+      end
+    end
+  end
+
+  def test_massive_files_err_gracefully
+    base_text = ->(itr) {
+      %|<p data-sourcepos="#{itr}:1-#{itr}:4"><sup data-sourcepos="#{itr}:1-#{itr}:4" class="footnote-ref"><a href="#fn-#{itr}" id="fnref-#{itr}" data-footnote-ref>#{itr}</a></sup></p>|
+    }
+
+    str = []
+    100000.times do |itr|
+      str << base_text.call(itr)
+    end
+    html = str.join("\n")
+
+    sanitizer_config = Selma::Sanitizer.new(Selma::Sanitizer::Config::RELAXED)
+    rewriter = Selma::Rewriter.new(sanitizer: sanitizer_config, handlers: [RemoveLinkClass.new, RemoveIdAttributes.new, BaseRemoveRel.new])
+    rewriter.rewrite(html)
+  end
 end
