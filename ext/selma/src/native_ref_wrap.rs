b24b5af5c0f90364afbfbbeeee32b3f47f96f648
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2024-07-31 08:33:45 -0400

    make it make sense

--- ext/selma/src/native_ref_wrap.rs
+++ ext/selma/src/native_ref_wrap.rs
@@ -3,7 +3,8 @@ use std::{
     sync::{Arc, Mutex},
 };
 
-// NOTE: this was taken from https://github.com/cloudflare/lol-html/blob/1a1ab2e2bf896f815fe8888ed78ccdf46d7c6b85/js-api/src/lib.rs#LL38
+// NOTE: this was inspired from
+// https://github.com/worker-tools/html-rewriter-wasm/blob/92bafdfa34c809c37036f57cb282184cada3bbc9/src/handlers.rs
 
 pub struct Anchor<'r> {
     poisoned: Arc<Mutex<bool>>,
@@ -19,36 +20,26 @@ impl<'r> Anchor<'r> {
     }
 }
 
-// impl Drop for Anchor<'_> {
-//     fn drop(&mut self) {
-//         *self.poisoned.lock().unwrap() = true;
-//     }
-// }
+impl Drop for Anchor<'_> {
+    fn drop(&mut self) {
+        *self.poisoned.lock().unwrap() = true;
+    }
+}
 
-// NOTE: wasm_bindgen doesn't allow structures with lifetimes. To workaround that
-// we create a wrapper that erases all the lifetime information from the inner reference
+// NOTE: So far as I understand it, there's no great wya to work between lol_html's lifetimes and FFI.
+// To work around that, we create a wrapper that erases all the lifetime information from the inner reference
 // and provides an anchor object that keeps track of the lifetime in the runtime.
 //
 // When anchor goes out of scope, wrapper becomes poisoned and any attempt to get inner
 // object results in exception.
+#[derive(Clone)]
 pub struct NativeRefWrap<R> {
     inner_ptr: *mut R,
     poisoned: Arc<Mutex<bool>>,
 }
 
 impl<R> NativeRefWrap<R> {
-    pub fn wrap<I>(inner: &I) -> (Self, Anchor) {
-        let wrap = NativeRefWrap {
-            inner_ptr: inner as *const I as *mut R,
-            poisoned: Arc::new(Mutex::new(false)),
-        };
-
-        let anchor = Anchor::new(Arc::clone(&wrap.poisoned));
-
-        (wrap, anchor)
-    }
-
-    pub fn wrap_mut<I>(inner: &mut I) -> (Self, Anchor) {
+    pub fn wrap<I>(inner: &mut I) -> (Self, Anchor) {
         let wrap = NativeRefWrap {
             inner_ptr: inner as *mut I as *mut R,
             poisoned: Arc::new(Mutex::new(false)),
@@ -57,6 +48,18 @@ impl<R> NativeRefWrap<R> {
         let anchor = Anchor::new(Arc::clone(&wrap.poisoned));
 
         (wrap, anchor)
+    }
+
+    fn assert_not_poisoned(&self) -> Result<(), &'static str> {
+        if self.is_poisoned() {
+            Err("The object has been freed and can't be used anymore.")
+        } else {
+            Ok(())
+        }
+    }
+
+    pub fn is_poisoned(&self) -> bool {
+        *self.poisoned.lock().unwrap()
     }
 
     pub fn get(&self) -> Result<&R, &'static str> {
@@ -69,14 +72,5 @@ impl<R> NativeRefWrap<R> {
         self.assert_not_poisoned()?;
 
         Ok(unsafe { self.inner_ptr.as_mut() }.unwrap())
-    }
-
-    fn assert_not_poisoned(&self) -> Result<(), &'static str> {
-        let lock = self.poisoned.lock().unwrap();
-        if *lock {
-            Err("The object has been freed and can't be used anymore.")
-        } else {
-            Ok(())
-        }
     }
 }
